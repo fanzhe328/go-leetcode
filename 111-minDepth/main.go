f609ec2eed89136726ede560269b089f71cbfffc
allenfan <allenfan@tencent.com>
2021-03-19 01:44:18 +0800

    update

---
+++ 111-minDepth/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	fmt.Println()
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	minD := math.MaxInt32
+	if root.Left != nil {
+		minD = min(minDepth(root.Left), minD)
+	}
+	if root.Right != nil {
+		minD = min(minDepth(root.Right), minD)
+	}
+	return minD + 1
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
