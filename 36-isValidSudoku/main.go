bcce7cceab0bb0ac92f610ddf7a7ad2815bd74b6
allenfan <allenfan@tencent.com>
2021-03-28 19:46:30 +0800

    update36

--- 36-isValidSudoku/main.go
+++ 36-isValidSudoku/main.go
@@ -5,30 +5,27 @@ import (
 )
 
 func main() {
-	a := 7
-	fmt.Println(a & 8)
+	a := 1 << 3
+	fmt.Println(a)
 }
 
 func isValidSudoku(board [][]byte) bool {
 	lens := len(board)
-	box := make([][]int, lens+1)
-	for i := 0; i < lens+1; i++ {
-		box[i] = make([]int, lens+1)
-	}
+	row, col, box := make([]uint32, lens+1), make([]uint32, lens+1), make([]uint32, lens+1)
 
-	for i := 0; i < len(board); i++ {
+	for i := 0; i < lens; i++ {
 		for j := 0; j < len(board[i]); j++ {
 			if board[i][j] == '.' {
 				continue
 			}
 
-			x := board[i][j]
-			if box[i][x-'0']&1 != 0 || box[j][x-'0']&2 != 0 || box[i/3*3+j/3][x-'0']&4 != 0 {
+			x := uint32(1) << (board[i][j] - '0')
+			if row[i]&x != 0 || col[j]&x != 0 || box[i/3*3+j/3]&x != 0 {
 				return false
 			}
-			box[i][x-'0'] |= 1
-			box[j][x-'0'] |= 2
-			box[i/3*3+j/3][x-'0'] |= 4
+			row[i] |= x
+			col[j] |= x
+			box[i/3*3+j/3] |= x
 		}
 	}
 	return true
